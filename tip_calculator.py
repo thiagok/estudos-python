4086c43c9d488a523319c50bf73a8679d4430e62
ObjMac01 <objmac01@ObjMac01.local>
2019-04-10 15:42:34 -0300

    Tip Calculator

---
+++ tip_calculator.py
@@ -0,0 +1,14 @@
+meal = 44.50
+tax = 6.75
+tip = 0.15
+
+# let's see how much I have in wallet
+cash_in_wallet = 7
+
+# oh no, that's for emergency
+cash_in_wallet = 0
+
+meal += meal * tax
+total = meal * tip
+
+print total
